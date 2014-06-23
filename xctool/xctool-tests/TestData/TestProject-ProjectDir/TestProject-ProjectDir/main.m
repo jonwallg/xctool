38c4c5154f0a471f4a79702b04e9ee481d06647f
Jon Wall <jonwall@google.com>
2014-06-23 12:06:58 -0400

    test project with custom projectDirPath

---
+++ xctool/xctool-tests/TestData/TestProject-ProjectDir/TestProject-ProjectDir/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestProject-ProjectDir
+//
+//  Created by Jon Wall on 6/23/14.
+//  Copyright (c) 2014 Jon Wall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
---
+++ xctool/xctool-tests/TestData/TestProject-ProjectDir/TestProject-ProjectDirTests/TestProject_ProjectDirTests.m
@@ -0,0 +1,34 @@
+//
+//  TestProject_ProjectDirTests.m
+//  TestProject-ProjectDirTests
+//
+//  Created by Jon Wall on 6/23/14.
+//  Copyright (c) 2014 Jon Wall. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestProject_ProjectDirTests : XCTestCase
+
+@end
+
+@implementation TestProject_ProjectDirTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testPass
+{
+    NSLog(@"Pass");
+}
+
+@end
